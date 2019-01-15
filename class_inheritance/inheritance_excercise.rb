e6b3fe108ba6296efda9cb960fdbb22f96d2eb52
Aram Sargsyan <aramsargsyan9902gmail.com>
2019-01-14 23:54:44 -0800

    Fix Manager#bonus bug

--- class_inheritance/inheritance_excercise.rb
+++ class_inheritance/inheritance_excercise.rb
@@ -1,39 +1,44 @@
 class Employee
-  attr_accessor :name, :title, :salary, :boss, :employees
-  def initialize(name,salary,title,boss, employees=[])
-    @name, @salary, @title, @boss, @employees = name, salary, title, boss, employees
+  attr_accessor :name, :title, :salary, :boss
+  def initialize(name,salary,title,boss)
+    @name, @salary, @title, @boss = name, salary, title, boss
   end
 
   def bonus(multiplier)
     salary * multiplier
   end
-  
+
 end
 
-class Manager < Employee 
+class Manager < Employee
+  attr_reader :employees
+  def initialize(name,salary,title,boss,employees)
+    @employees = employees
+    super(name,salary,title,boss)
+  end
 
    def bonus(multiplier)
-    sub_salaries.sum * multiplier
+    all_subordinates.map {|sub| sub.salary}.inject(0,:+) * multiplier
    end
-private
-  def sub_salaries
+
+
+
+  def all_subordinates
     subs = []
     employees.each do |el|
-      if el.class != Array
+      if el.class == Manager
         subs << el
+        subs += el.all_subordinates
       else
-        subs << bonus(multiplier)
+        subs << el
       end
     end
-    subs.map {|el| el.salary}
+    subs
   end
 
 end
 
- s = Employee.new("Shawana", 12000,"TA","Darren")
- s1 = Employee.new("David", 10000,"TA","Darren")
-manager = Manager.new("Darren", 78000,"TA_manager","Ned", [s,s1])
-manager = Manager.new("Ned", 78000,"TA_manager","Ned", [s,s1])
-#  manager.bonus(4)
-#  s1.bonus(3)
- manager.employees
+ #s = Employee.new("Shawana", 12000,"TA","Darren")
+ #s1 = Employee.new("David", 10000,"TA","Darren")
+#manager = Manager.new("Darren", 78000,"TA_manager","Ned", [s,s1])
+#founder = Manager.new("Ned", 100000,"founder","Ned", [manager])
